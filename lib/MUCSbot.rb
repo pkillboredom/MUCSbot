5777332b3b5314bc45e4bbb6926083eda2dc533a
Luke Tomkus <luke@pkillboredom.com>
2017-02-21 18:54:05 -0500

    generates config file if it does not exist

--- lib/MUCSbot.rb
+++ lib/MUCSbot.rb
@@ -9,10 +9,23 @@ require 'discordrb'
 require 'yaml'
 
 module MUCSbot
+  #Generates config file if one does not exist
+  unless File.exists?('config.yaml')
+    begin
+      default_config = File.open('example-config.yaml')
+      config = File.new('config.yaml', 'w')
+      config.write(default_config)
+    rescue
+      abort("There is no config.yaml and one could not be made from example-config.yaml\n" +
+        "Please download a clean copy of MUCSbot or check your permissions.")
+    end
+  end
+
   #Load classes
   Dir["#{File.dirname(__FILE__)}/*.rb"].each { |file| require file }
 
   CONFIG = YAML.load_file('config.yaml')
+
   BOT = Discordrb::Commands::CommandBot.new(
       token: CONFIG['token'],
       client_id: 276192706412281858,
