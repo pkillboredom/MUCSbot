1720b31f1f242970bc4cd505c32c403517389aa6
Luke Tomkus <luke@pkillboredom.com>
2017-05-22 20:59:33 -0400

    fixed a small error

--- lib/MUCSbot.rb
+++ lib/MUCSbot.rb
@@ -59,9 +59,9 @@ module MUCSbot
       #Connects to voice channels
       CONFIG['sound']['servers'].each do |server|
         begin
-          BOT.voice_connect(CONFIG['sound']['channel'])
+          BOT.voice_connect(server['channel'])
         rescue
-          puts("#{server}.#{server['channel']} failed to connect. Voice will not work on that server.")
+          puts("#{server['channel']} failed to connect. Voice will not work on that server.")
         end
       end
     end
