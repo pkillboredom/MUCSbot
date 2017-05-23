b32b8526f44b9327243a92c29ba1d344b065051d
Luke Tomkus <luke@pkillboredom.com>
2017-05-22 23:36:48 -0400

    I really need to just set up ftp so that I dont have to push every time

--- lib/commands/sound.rb
+++ lib/commands/sound.rb
@@ -20,7 +20,7 @@ module MUCSbot
       min_args: 1,
       max_args: 1) do |event, *text|
         file = text[0]
-        res = sounds.find { |x| x.include? file}
+        res = $sounds.find { |x| x.include? file}
         puts res
         unless res.nil?
           event.voice.play_file(res)
