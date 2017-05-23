32f3387a6e6bc0e457bf2ab63a1fbc4355337566
Luke Tomkus <luke@pkillboredom.com>
2017-05-23 19:52:13 -0400

    I fixed my environment so no more frantic commits. Implemented listsounds, play can generate a selector, and the system is now case insensitive

--- lib/commands/sound.rb
+++ lib/commands/sound.rb
@@ -7,25 +7,91 @@ module MUCSbot
       PlayDescription = ""
       PlayUsage = ""
 
+      $sounds = nil
+      $sound_list = []
       #Map of playable sound files
       def self.reload_sounds
-        $sounds = Dir.glob("#{File.dirname(__FILE__)}/sounds/**/*").select { |f| File.file?(f) }
+        $sounds = Dir.glob("#{File.dirname(__FILE__)}/#{CONFIG['sound']['sound-folder']}/**/*").select { |f| File.file?(f) }
+        $sounds = $sounds.sort
+        page_counter = 0
+        item_counter = 0
+        $sound_list = []
+        $sound_list[0] = []
+        $sounds.each do |x|
+          unless item_counter >= 10
+            $sound_list[page_counter][item_counter] = "#{File.expand_path(x)[/(?<=#{CONFIG['sound']['sound-folder']}\/).*$/]}"
+            item_counter += 1
+          else
+            item_counter = 0
+            page_counter += 1
+            $sound_list[page_counter] = []
+          end
+        end
       end
-      $sounds = nil
       reload_sounds
 
       command(:play,
-      description:PlayDescription,
-      usage:PlayUsage,
-      min_args: 1,
-      max_args: 1) do |event, *text|
-        file = text[0]
-        res = $sounds.find { |x| x.include? file}
-        puts res
-        unless res.nil?
-          event.voice.play_file(res)
-          event.voice.stop_playing
+              description:PlayDescription,
+              usage:PlayUsage,
+              min_args: 1,
+              max_args: 2) do |event, *text|
+        if text[1].nil?
+          file_input = text[0]
+          search_result = $sounds.grep(/(?<=#{CONFIG['sound']['sound-folder']}\/).*#{file_input}.*/i)
           res = nil
+          if search_result.length > 1
+            if search_result.length > 15
+              event.send("Search not specific enough. Please try narrowing your query.")
+            else
+              string_to_send = "Please append a selection to your command to select\n" +
+                  "e.g. /play #{file_input} 2\n```"
+              counter = 1
+              selector = {}
+              search_result.each do |x|
+                selector[counter] = x
+                string_to_send += "#{counter}) #{File.expand_path(x)[/(?<=#{CONFIG['sound']['sound-folder']}\/).*$/]}\n"
+                counter += 1
+              end
+              string_to_send += '```'
+              event.send(string_to_send)
+            end
+          else
+            res = search_result.first
+          end
+          puts res
+          unless res.nil?
+            puts "Hitting play as lone file"
+            event.voice.play_file(res)
+            event.voice.stop_playing
+            res = nil
+          end
+        else
+          file_input = text[0]
+          arg1 = text[1].to_i
+          search_result = $sounds.grep(/(?<=#{CONFIG['sound']['sound-folder']}\/).*#{file_input}.*/i)
+          res = nil
+          if search_result.length > 0
+            if search_result.length > 15
+              event.send("Search not specific enough. Please try narrowing your query.")
+            else
+              counter = 1
+              selector = {}
+              search_result.each do |x|
+                selector[counter] = x
+                counter += 1
+              end
+              res = selector[arg1]
+            end
+          else
+            event.send("Your second argument was invalid.")
+          end
+          puts res
+          unless res.nil?
+            puts "Hitting play as a selector"
+            event.voice.play_file(res)
+            event.voice.stop_playing
+            res = nil
+          end
         end
       end
 
@@ -36,7 +102,17 @@ module MUCSbot
 
       command(:listsounds) do |event, *text|
         event.send("PMing sounds.")
-        event.author.pm($sounds)
+        unless text[0].nil?
+          string_to_pm = '```'
+          $sound_list[text[0].to_i].each {|x| string_to_pm += "#{x}\n"}
+          string_to_pm += '```'
+          event.author.pm(string_to_pm)
+        else
+          string_to_pm = '```'
+          $sound_list[0].each {|x| string_to_pm += "#{x}\n"}
+          string_to_pm += '```'
+          event.author.pm(string_to_pm)
+        end
       end
 
       command(:stop) do |event|
