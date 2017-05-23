8aa4fa67a5fee4d057a357364168911528b5e7a4
Luke Tomkus <luke@pkillboredom.com>
2017-05-22 23:09:50 -0400

    quotes?

--- lib/commands/sound.rb
+++ lib/commands/sound.rb
@@ -8,38 +8,8 @@ module MUCSbot
       PlayUsage = ""
 
       #Map of playable sound files
-      sounds = Dir.glob("./sounds/**/*")
-      puts CONFIG['sound']['sound-folder']
+      sounds = Dir.glob('./sounds/**/*')
       puts sounds
-
-=begin
-      #Connects to voice channels
-      servers = MUCSbot::BOT.servers
-      CONFIG['sound']['servers'].each do |serverid|
-
-        #try to find the server
-        the_server = nil
-        servers.each do |server|
-          if server.id == serverid
-            the_server = server
-          end
-        end
-
-        #try to find the channel
-        the_channel = nil
-        the_server.channels.each do |channel|
-          if channel.id == serverid['channel']
-            the_channel = channel
-          end
-        end
-
-        begin
-          res = MUCSbot::BOT.voice_connect(the_channel)
-        rescue
-          puts("#{serverid}.#{serverid['channel']} failed to connect. Voice will not work on that server.")
-        end
-      end
-=end
 
       command(:play,
       description:PlayDescription,
